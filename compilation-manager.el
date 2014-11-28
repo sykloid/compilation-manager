51f4b4d1c8ff9c9406752760cabf9d90f20a3c6b
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-11-28 06:51:05 -0500

    Add customization specification for profiles.

--- compilation-manager.el
+++ compilation-manager.el
@@ -8,4 +8,22 @@
 
   :group 'compilation)
 
+(defcustom compilation-manager-profiles nil
+  "Profile specifications for `compilation-manager'.
+
+An alist of compilation profile names and definitions. Each profile is a plist
+of:
+
+- `:compile-command' :: The value of `compile-command'.
+- `:default-directory' :: The value of `default-directory'.
+- `:search-path' :: The value of `compilation-search-path'.
+- `:interactive' :: Whether or not the compilation buffer is interactive."
+
+  :type '(alist :key-type string
+                :value-type (plist
+                             :options (:compile-command
+                                       :default-directory
+                                       :interactive
+                                       :search-path))))
+
 (provide 'compilation-manager)
