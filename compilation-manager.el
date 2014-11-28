4aafaf528c8dec811ff13f7c330b36b6c2bad9d7
P.C. Shyamshankar <sykora@lucentbeing.com>
2014-11-28 06:47:31 -0500

    Add preamble, customization group.

---
+++ compilation-manager.el
@@ -0,0 +1,11 @@
+;;; compilation-manager.el - profile management for emacs' compilation-mode.
+;;; P.C. Shyamshankar <sykora@lucentbeing.com>
+
+;;; Commentary:
+
+(defgroup compilation-manager nil
+  "Customization for `compilation-manager'."
+
+  :group 'compilation)
+
+(provide 'compilation-manager)
